97100710af9a2ff212b4a7bfa96a3ac84ab88e68
gustavolkbr <gustavo.lnkbr@gmail.com>
2025-06-19 12:12:26 -0300

    update

--- python/lista1/q06.py
+++ python/lista1/q06.py
@@ -1,6 +1,7 @@
 vehicle_price = float(input("what's the vehicle price?: ")) 
 numof_installment = int(input("what the number of installments?: "))
 percent = float(input("what's the interest rate %?:" ))
+
 interest_rate = percent/100
 
 installment = (vehicle_price/numof_installment)*(1+interest_rate)
